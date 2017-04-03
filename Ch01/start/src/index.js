d33b5a785a75587f4de74a89f3a785554092ed53
douglas.araujo <doug.ramalho@gmail.com>
2017-04-03 08:27:09 -0300

    Initially subscribe and unsubscribe methods

--- Ch01/start/src/index.js
+++ Ch01/start/src/index.js
@@ -1,82 +1,19 @@
 import C from './constants'
 import appReducer from './store/reducers'
-import initialState from './initialState.json'
+import { createStore } from "redux"
 
-let state = initialState
+const initialState = (localStorage["redux-store"]) ? 
+	JSON.parse(localStorage["redux-store"]) :
+	{};
 
-console.log(`
+const store = createStore(appReducer, initialState);
 
-	Initial state
-	=============
-	goal: ${state.goal}
-	resorts: ${JSON.stringify(state.allSkiDays)}
-	fetching: ${state.resortNames.fetching}
-	suggestions: ${state.resortNames.suggestions}
+window.store = store;
 
-`)
+store.subscribe(() => {
+	
+	const state = JSON.stringify(store.getState());
+	localStorage['redux-store'] = state;
 
-state = appReducer(state, {
-	type: C.SET_GOAL,
-	payload: 2
 })
 
-state = appReducer(state, {
-	type: C.ADD_DAY,
-	payload: {
-		"resort": "Mt Shasta",
-		"date": "2016-10-28",
-		"powder": false,
-		"backcountry": true
-	}
-})
-
-state = appReducer(state, {
-	type: C.CHANGE_SUGGESTIONS,
-	payload: ["Mt Tallac", "Mt Hood", "Mt Shasta"]
-})
-
-console.log(`
-
-	Next state
-	=============
-	goal: ${state.goal}
-	resorts: ${JSON.stringify(state.allSkiDays)}
-	fetching: ${state.resortNames.fetching}
-	suggestions: ${state.resortNames.suggestions}
-
-`)
-
-
-
-//import C from "./constants"
-//import { allSkiDays } from "./store/reducers"
-
-//const state = [
-//	{
-//		"resort": "Kirkwood",
-//		"date": "2016-12-15",
-//		"powder": true,
-//		"backcountry": false
-//	},
-//	{
-//		"resort": "Boreal",
-//		"date": "2016-12-16",
-//		"powder": false,
-//		"backcountry": false
-//	}
-//]
-
-//const action = {
-//	type: C.REMOVE_DAY,
-//	payload: "2016-12-15"
-//}
-
-//const nextState = allSkiDays(state, action)
-
-//console.log(`
-
-//	intial state: ${JSON.stringify(state)}
-//	action: ${JSON.stringify(action)}
-//	new state: ${JSON.stringify(nextState)}
-
-//`)
